9f943f869b8bfcefed9b1da2483be2635bb14ef3
Cravzew <korolnyy2@mail.ru>
2023-04-21 21:52:43 +0300

    animation added (ssr)

--- src/components/Telephone/Main/CellCard/CellCard.tsx
+++ src/components/Telephone/Main/CellCard/CellCard.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from "@/components/Telephone/Main/main.module.scss";
 import Image from "next/image";
 import {ICellCard} from "@/store/slice/dataReducer";
+import {motion} from "framer-motion";
 
 function CellCard(props: ICellCard) {
 
@@ -12,7 +13,16 @@ function CellCard(props: ICellCard) {
     } = props
 
     return (
-        <li className={styles.main__list__item}>
+        <motion.li
+            className={styles.main__list__item}
+            initial={{opacity: 0}}
+            animate={{opacity: 1}}
+            exit={{opacity: 0}}
+            transition={{duration: 0.5}}
+            drag="x"
+            layout
+            dragConstraints={{left: 0, right: 0}}
+        >
             <Image src={image} alt={`${image}`}/>
             <div className={styles.main__list__item__content}>
                 <h2 className={styles.main__list__item__content__header}>
@@ -22,7 +32,7 @@ function CellCard(props: ICellCard) {
                     {description}
                 </p>
             </div>
-        </li>
+        </motion.li>
     );
 }
 
--- src/components/Telephone/Main/Main.tsx
+++ src/components/Telephone/Main/Main.tsx
@@ -2,8 +2,8 @@ import React, {useEffect} from 'react';
 import styles from './main.module.scss'
 import {useAppDispatch, useAppSelector} from "@/store/store";
 import {pushLife, removeLife} from "@/store/slice/dataReducer";
-import {CSSTransition, TransitionGroup} from 'react-transition-group'
 import CellCard from "@/components/Telephone/Main/CellCard/CellCard";
+import {AnimatePresence} from "framer-motion";
 
 function Main() {
     const cells = useAppSelector(state => state.data.stats)
@@ -21,7 +21,7 @@ function Main() {
     useEffect(() => {
         const interval = setInterval(() => {
             dispatch(removeLife())
-        }, 5000)
+        }, 3000)
         return () => {
             clearInterval(interval)
         }
@@ -29,24 +29,15 @@ function Main() {
 
     return (
         <main className={styles.main}>
-            <TransitionGroup
-                component='ul'
-                className={styles.main__list}
-            >
-                {cells.map((i) =>
-                    <CSSTransition
-                        timeout={500}
-                        classNames="fade"
-                        key={Math.round(Math.random() * 100000)}
-                        unmountOnExit
-                        mountOnEnter
-                    >
+            <ul className={styles.main__list}>
+                <AnimatePresence>
+                    {cells.map((i) =>
                         <CellCard key={Math.round(Math.random() * 100000)} image={i.image} header={i.header}
                                   description={i.description}
                         />
-                    </CSSTransition>
-                )}
-            </TransitionGroup>
+                    )}
+                </AnimatePresence>
+            </ul>
             <div className={styles.main__button_content}>
                 <button onClick={handlePush} className={styles.main__button_content__button}>
                     Сотворить
